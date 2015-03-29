adfcc39cc86d74458ed3d6d50bff559953408d2c
Barack Hussein <b.h.obama-2@yandex.ru>
2015-03-29 20:32:03 +0300

    fix

--- templates/list.php
+++ templates/list.php
@@ -11,33 +11,33 @@
   <table class="table table-bordered">
 
     <tr>
-      <?php foreach ($orderColumns as $linkName => $url) { ?>  
+      <?php foreach ($orderColumns as $linkName => $url): ?>  
         <th>
           <a href="<?= h($url) ?>"><?= $linkName ?></a> 
         </th>
-      <?php } ?>
+      <?php endforeach; ?>
     </tr>
 
-    <?php foreach ($students as $student) { ?>
+    <?php foreach ($students as $student) : ?>
       <tr>
         <td><?= highlighting(h($student->getName()), h($userSearch)) ?></td>
         <td><?= highlighting(h($student->getSurname()), h($userSearch)) ?> </td>
         <td><?= highlighting(h($student->getGroupNumber()), h($userSearch)) ?> </td>
         <td><?= highlighting(h($student->getScores()), h($userSearch)) ?> </td>
       </tr>
-    <?php } ?>
+    <?php endforeach; ?>
 
   </table>
 
   <nav>
     <ul class="pagination">
-      <?php foreach ($countPage as $linkName => $url) { ?>
-        <li <?php if ($startRecord == ($linkName - 1) * $recordPerPage): ?>
+      <?php foreach ($countPage as $pageNumber => $url) : ?>
+        <li <?php if ($startRecord == ($pageNumber - 1) * $recordPerPage): ?>
             class="active"
           <?php endif; ?>>
-          <a href="<?= h($url) ?>"><?= $linkName ?></a>
+          <a href="<?= h($url) ?>"><?= $pageNumber ?></a>
         </li>
-      <?php } ?>
+      <?php endforeach; ?>
     </ul>
   </nav>
 </div>
